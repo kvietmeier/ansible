038d88975d76312b9c4459b5227bcae041674938
kvietmeier <karlv@storagenet.org>
2019-02-17 14:05:45 -0800

    Reconfiguring Layout

---
+++ bootstrap/files/cpu_gov.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+GOVERNOR=performance
+for CPUFREQ in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+  do
+    [ -f $CPUFREQ ] || continue
+    echo -n $GOVERNOR > $CPUFREQ
+  done
