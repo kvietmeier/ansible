53656869bcfb120caa2a02f4609fc3d1927d2853
kvietmeier <karlv@storagenet.org>
2023-10-10 14:18:26 +0000

    minor tweaks

--- roles/voltdb/files/demo/kill_bench.sh
+++ roles/voltdb/files/demo/kill_bench.sh
@@ -7,14 +7,16 @@
 PID=$(ps -deaf | grep ChargingDemoTransactions.jar  | grep -v grep | awk '{ print $2 }')
 
 function kill_threads () {
+
 until [ -z $PID ]
-    do
-        echo "Killing $PID"
-        kill -9 $PID
-        sleep 2
-        PID=$(ps -deaf | grep ChargingDemoTransactions.jar  | grep -v grep | awk '{ print $2 }')
-        echo $PID
+  do
+    echo "Killing $PID"
+    kill -9 $PID
+    sleep 2
+    PID=$(ps -deaf | grep ChargingDemoTransactions.jar  | grep -v grep | awk '{ print $2 }')
+    echo $PID
   done
+
 }
 
 kill_threads
