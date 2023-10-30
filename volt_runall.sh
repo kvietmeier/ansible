858a59cbffd28285a671555324f682450a8ce70d
kvietmeier <karlv@storagenet.org>
2023-10-30 15:45:47 +0000

    Update wrapper

--- volt_runall.sh
+++ volt_runall.sh
@@ -3,7 +3,11 @@
 #
 # Use: Wrapper for setting up VoltDB
 #
-# Hard coded for 4 nodes+mgmt node.
+# Node list is set in $volt_nodes (source a file instead?)
+# Mgmt node is vdb-01
+#
+# NOTE: "~" gets passed as a string, "$HOME" gets interepted locally - then passed
+#
 
 ###---- Vars
 inventory=${HOME}/ansible/inventory
@@ -12,8 +16,13 @@ volt_ver=11.4
 volt_user=ubuntu
 volt_bin="/home/ubuntu/voltdb-ent-${volt_ver}/bin/voltdb"
 demo_dir="/home/ubuntu/chargingdb"
+config_file="/home/ubuntu/demo_cluster_config.xml"
+license_file="/home/ubuntu/license.xml"
 start_cmd="nohup $volt_bin start --dir=${demo_dir} --host=${volt_host} > $HOME/voltstart.out 2> $HOME/voltstart.err < /dev/null &"
-volt_host="vdb-02,vdb-03,vdb-04,vdb-05"
+volt_nodes=("vdb-02" "vdb-03" "vdb-04" "vdb-05")
+
+###=============================== Should not need to edit below this line ====================================###
+
 
 ###---- Functions
 # Using tags in Playbook to break up tasksd for hosts.
@@ -46,7 +55,7 @@ function init () {
   echo "    Initializing Databases"
   echo "###====================================###"
   echo ""
-  ansible voltnodes -i $inventory -m shell -a "/home/ubuntu/voltdb-ent-${volt_ver}/bin/voltdb init --dir=~/chargingdb --config=~/demo_cluster_config.xml --license=~/license.xml --force" --become-user ubuntu
+  ansible voltnodes -i $inventory -m shell -a "$volt_bin init --dir=$demo_dir --config=$demo_file --license=$license_file --force" --become-user $volt_user
 }
 
 sleep 10
@@ -60,10 +69,10 @@ function start_volt () {
   echo "###====================================###"
   echo ""
 
-  for node in 2 3 4 5 ; do	 
+  for node in ${volt_nodes[@]} ; do	 
     # This works - need both nohup and move & to the end
     # Works equally as well as a straight SSH pass through
-    ansible vdb-0${node} -m shell -a "nohup /home/ubuntu/voltdb-ent-${volt_ver}/bin/voltdb start --dir=/home/ubuntu/chargingdb --host=vdb-02,vdb-03,vdb-04,vdb-05 > $HOME/voltstart.out 2> $HOME/voltstart.err < /dev/null &"
+    ansible $node -m shell -a "nohup $volt_bin start --dir=$demo_dir --host=$volt_hosts > ~/voltstart.out 2> ~/voltstart.err < /dev/null &" --become-user $volt_user
     sleep 10
   done
 
@@ -73,10 +82,23 @@ function start_volt () {
 
 }
 
+function setup_prometheus_export () {
+
+  for node in ${volt_nodes[@]} ; do	 
+    # Need to setup export of stats on each DB node
+    ansible $node -m shell -a "~/bin/voltdbprometheusbl_start.sh 2> ~/voltprometheusbl.err < /dev/null &" --become-user $volt_user
+    sleep 3
+  done
+
+}
+
+
+
 ###---- END Functions
 
 
 # Call functions
 #playbooks
-init
-start_volt
+#init
+#start_volt
+setup_prometheus_export
