9094f37b956e21efbaa068a00fb848da49215e8b
kvietmeier <karlv@storagenet.org>
2025-04-04 17:49:17 +0000

    update

--- mount_nfs.sh
+++ mount_nfs.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Ansible ad hoc commands to setup NFS mounts on clients
-3 Brute force - no error checking.
+# Brute force - no error checking.
 
 for i in $(seq 1 6); do
     #echo "ansible -i ./inventory clients -a \"mkdir /mount/nfs${i}\""
-    ansible -i ./inventory clients -a "mkdir /mount/nfs${i}"
+    ansible -i ./inventory clients -a "mkdir -p /mount/share${i}"
 done
 
 j=1
-for i in $(seq 80 85); do
-    ansible -i ./inventory clients -a "mount -t nfs 10.100.2.${i}:/nfs${j} /mount/nfs${j}"
+for i in $(seq 1 6); do
+    #ansible -i ./inventory clients -a "mount -t nfs 10.100.7.${i}:/nfs${j} /mount/nfs${j}"
+    ansible -i ./inventory clients -a "mount -o proto=tcp,vers=3,nconnect=8 mountvip.arrakis.org:/share${j} /mount/share${j}"
     ((j++))
 done
