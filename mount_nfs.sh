a01b84557cc665e9d675057921dad405dea0384d
kvietmeier <karlv@storagenet.org>
2025-04-07 06:57:54 +0000

    updsted viewpolicy code

--- mount_nfs.sh
+++ mount_nfs.sh
@@ -10,6 +10,9 @@ done
 j=1
 for i in $(seq 1 6); do
     #ansible -i ./inventory clients -a "mount -t nfs 10.100.7.${i}:/nfs${j} /mount/nfs${j}"
-    ansible -i ./inventory clients -a "mount -o proto=tcp,vers=3,nconnect=8 mountvip.arrakis.org:/share${j} /mount/share${j}"
+    ansible -i ./inventory clients -a "mount -t nfs -o proto=tcp,vers=3,nconnect=8 mountvip.arrakis.org:/share${j} /mount/share${j}"
     ((j++))
 done
+
+
+#mount -t nfs -o proto=tcp,vers=3,nconnect=8,remoteports=10.132.0.211~10.132.0.214~10.132.0.221~10.132.0.229~10.132.0.230~10.132.0.233 mountvip.arrakis.org:/share${j} /mount/share${j}
