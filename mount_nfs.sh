cfd0b340578b85a6df1ca8491d33cac59089ef89
kvietmeier <karlv@storagenet.org>
2025-04-09 04:32:57 +0000

    update

--- mount_nfs.sh
+++ mount_nfs.sh
@@ -2,17 +2,18 @@
 # Ansible ad hoc commands to setup NFS mounts on clients
 # Brute force - no error checking.
 
-for i in $(seq 1 6); do
+#for i in $(seq 1 6); do
     #echo "ansible -i ./inventory clients -a \"mkdir /mount/nfs${i}\""
-    ansible -i ./inventory clients -a "mkdir -p /mount/share${i}"
-done
+    #ansible -i ./inventory clients -a "mkdir -p /mount/share${i}"
+#done
 
 j=1
 for i in $(seq 1 6); do
     #ansible -i ./inventory clients -a "mount -t nfs 10.100.7.${i}:/nfs${j} /mount/nfs${j}"
-    ansible -i ./inventory clients -a "mount -t nfs -o proto=tcp,vers=3,nconnect=8 mountvip.arrakis.org:/share${j} /mount/share${j}"
+    #ansible -i ./inventory clients -a "mount -t nfs -o proto=tcp,vers=3,nconnect=8 mountvip.arrakis.org:/share${j} /mount/share${j}"
+    ansible -i ./inventory clients -a "mount -t nfs -o proto=tcp,vers=3,nconnect=8,remoteports=10.100.2.200-10.100.2.205 mountvip.arrakis.org:/share${j} /mount/share${j}"
     ((j++))
 done
 
 
-#mount -t nfs -o proto=tcp,vers=3,nconnect=8,remoteports=10.132.0.211~10.132.0.214~10.132.0.221~10.132.0.229~10.132.0.230~10.132.0.233 mountvip.arrakis.org:/share${j} /mount/share${j}
+# mount -t nfs -o proto=tcp,vers=3,nconnect=8,remoteports=10.132.0.211~10.132.0.214~10.132.0.221~10.132.0.229~10.132.0.230~10.132.0.233 mountvip.arrakis.org:/share${j} /mount/share${j}
